8095fea19ee4f226ea1c431d2cfe40435b6a5034
Rong <captrong@gooda.my>
2025-05-31 21:51:53 +0800

    feature/add button in navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,27 @@
 import React from "react";
-
+import {
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <main>
+      <div className="flex justify-between items-center">
+        <div>
+          <SignedOut>
+            <SignInButton />
+            <SignUpButton />
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default Navbar;
