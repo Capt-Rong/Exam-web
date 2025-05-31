cae6072e18ab6b1138ff3de27b91804d70be3fef
Rong <captrong@gooda.my>
2025-06-01 02:39:45 +0800

    feature/add-fake-card-note-page

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,5 +1,7 @@
+import SubjectCard from "@/components/SubjectCard";
 import { createClient } from "../../lib/supabase/server";
 import { cookies } from "next/headers";
+import { noteSubjects } from "@/constants";
 
 export default async function NotePage() {
   const cookieStore = cookies();
@@ -12,10 +14,25 @@ export default async function NotePage() {
   }
 
   return (
-    <div>
-      <h1>Notes Page</h1>
-      <pre>{JSON.stringify(notes, null, 2)}</pre>
-      {error && <p>Error loading notes. Check console.</p>}
-    </div>
+    <main>
+    <section className="container mx-auto px-6 py-8">
+      <h1 className="text-2xl font-bold text-gray-800 mb-8">
+        Practice Page - Questions
+      </h1>
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 md:gap-8">
+            {noteSubjects.map((subject) => (
+              <SubjectCard
+                key={subject.name}
+                subjectName={subject.name}
+                description={subject.description}
+                bgColorClass={subject.bgColorClass}
+                textColorClass={subject.textColorClass}
+                buttonColorClass={subject.buttonColorClass}
+                href={subject.href}
+              />
+            ))}
+          </div>
+    </section>
+  </main>
   );
 }
