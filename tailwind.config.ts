964e75e83443ff4aefe9f6bd95ef845f0b2ad6fe
Rong <captrong@gooda.my>
2025-06-01 14:49:34 +0800

    fix/markdown-size-in-tailwind-config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,22 +1,13 @@
 import type { Config } from "tailwindcss";
 import typography from "@tailwindcss/typography";
+// import { listenerCount } from "process";
+// import { markAssetError } from "next/dist/client/route-loader";
 
 const config: Config = {
-  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [typography],
-};
-
-module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/react-markdown/lib/react-markdown.js",
-    // Or if using `src` directory:
-    // "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
@@ -25,7 +16,7 @@ module.exports = {
           // This targets the base `prose` class
           css: {
             h1: {
-              fontSize: "2.5rem", // 40px
+              fontSize: "2rem", // 40px
               fontWeight: "700",
               lineHeight: "1.2",
               marginTop: "2em",
@@ -53,16 +44,104 @@ module.exports = {
               marginBottom: "0.5em",
             },
             h5: {
-              fontSize: "1.125rem", // 18px
+              fontSize: "1.2rem", // 18px
               fontWeight: "600",
               lineHeight: "1.4",
               marginTop: "1em",
               marginBottom: "0.5em",
             },
             h6: {
-              fontSize: "1rem", // 16px
+              fontSize: "1.125rem", // 16px
               fontWeight: "600",
               lineHeight: "1.4",
+              marginTop: "1em",
+              marginBottom: "0.5em",
+            },
+            p: {
+              fontSize: "1rem",
+              fontWeight: "400",
+              lineHeight: "1.5",
+              marginTop: "1em",
+              marginBottom: "0.5em",
+            },
+            ul: {
+              listStyleType: "disc",
+              marginLeft: "1.5em",
+              marginTop: "1em",
+              marginBottom: "0.5em",
+            },
+            li: {
+              listStyleType: "disc",
+              marginLeft: "1.5em",
+              marginTop: "0.5em",
+              marginBottom: "0.5em",
+              color: "#1E1E1E",
+              fontSize: "1rem",
+              fontWeight: "400",
+              lineHeight: "1.5",
+            },
+            ol: {
+              listStyleType: "decimal",
+              marginLeft: "1.5em",
+              marginTop: "1em",
+              marginBottom: "0.5em",
+            },
+            code: {
+              fontSize: "0.875rem",
+              fontWeight: "400",
+              lineHeight: "1.5",
+              marginTop: "1em",
+              marginBottom: "0.5em",
+            },
+            blockquote: {
+              fontSize: "1rem",
+              fontWeight: "400",
+              lineHeight: "1.5",
+              marginTop: "1em",
+              marginBottom: "0.5em",
+            },
+            a: {
+              color: "#007bff",
+              textDecoration: "underline",
+              "&:hover": {
+                color: "#0056b3",
+              },
+            },
+            img: {
+              maxWidth: "100%",
+              height: "auto",
+              marginTop: "1em",
+              marginBottom: "0.5em",
+            },
+            hr: {
+              borderColor: "#e0e0e0",
+              borderWidth: "1px",
+              marginTop: "2em",
+              marginBottom: "2em",
+            },
+            table: {
+              width: "100%",
+              marginTop: "1em",
+              marginBottom: "0.5em",
+            },
+            thead: {
+              backgroundColor: "#f0f0f0",
+              "& th": {
+                padding: "0.5em 1em",
+                textAlign: "left",
+              },
+            },
+            tbody: {
+              "& tr": {
+                "&:nth-child(even)": {
+                  backgroundColor: "#f0f0f0",
+                },
+              },
+            },
+            pre: {
+              fontSize: "0.875rem",
+              fontWeight: "400",
+              lineHeight: "1.5",
               marginTop: "1em",
               marginBottom: "0.5em",
             },
